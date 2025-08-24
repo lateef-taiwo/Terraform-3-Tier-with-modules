9966947a590a804c4e0c52a3bde9c7f0478a3e48
Lateef Taiwo <taiwolateef55@gmail.com>
2025-08-24 16:30:05 -0500

    added  .github/workflows/terraform-deploy.yaml and modified:   Root/backend.tf

--- Root/backend.tf
+++ Root/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
     bucket = "state-file-bucket-0056"
-    key    = "backend/test.tfstate"
+    key    = "backend/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
+    encrypt = true
     #dynamodb_table = "DynamoDB-state-lock"
   }
 }
